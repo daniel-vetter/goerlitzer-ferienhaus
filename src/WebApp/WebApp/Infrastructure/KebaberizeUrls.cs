cda8d79b596b1b43c45c3ba8841b4e1a5b7dbd31
Daniel Vetter <daniel.vetter86@gmail.com>
2021-05-31 01:51:02 +0200

    Removed Humanizer dependency

--- src/WebApp/WebApp/Infrastructure/KebaberizeUrls.cs
+++ src/WebApp/WebApp/Infrastructure/KebaberizeUrls.cs
@@ -1,5 +1,5 @@
-using Humanizer;
 using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using System.Text.RegularExpressions;
 
 namespace WebApp.Infrastructure
 {
@@ -8,7 +8,12 @@ namespace WebApp.Infrastructure
         public void Apply(PageRouteModel model)
         {
             foreach (var modelSelector in model.Selectors)
-                modelSelector.AttributeRouteModel.Template = modelSelector.AttributeRouteModel.Template.Kebaberize();
+                modelSelector.AttributeRouteModel.Template = Kebaberize(modelSelector.AttributeRouteModel.Template);
+        }
+
+        private string Kebaberize(string input)
+        {
+            return Regex.Replace(Regex.Replace(Regex.Replace(input, @"([\p{Lu}]+)([\p{Lu}][\p{Ll}])", "$1-$2"), @"([\p{Ll}\d])([\p{Lu}])", "$1-$2"), @"[-\s]", "-").ToLower();
         }
     }
 }
